26975ff50dc821ca522e08c1719f9c654aaa3098
Jakob Ovrum <jakobovrum@gmail.com>
2013-10-09 01:42:35 +0900

    Initial documentation for the diggler.attribute module

--- src/diggler/attribute.d
+++ src/diggler/attribute.d
@@ -3,18 +3,37 @@ module diggler.attribute;
 import std.typetuple : allSatisfy;
 import std.traits : isSomeString;
 
+// TODO: Use constructor functions for nicer documentation?
+
 // Command set attributes
+/**
+ * Command set attribute.
+ *
+ * Apply to a deriviate class of $(DPREF command, CommandSet)
+ * to set its categorical name.
+ */
 struct category
 {
 	package string value;
 }
 
 // Command attributes
+/**
+ * Command attribute.
+ *
+ * Apply to a command method to provide a description for the command.
+ */
 struct usage
 {
 	package string value;
 }
 
+/**
+ * Command attribute.
+ *
+ * Apply to a command method to provide alternative names for a command,
+ * that can be used to invoke it in chat.
+ */
 struct aliases
 {
 	package string[] value;
@@ -25,6 +44,22 @@ struct aliases
 	}
 }
 
+/**
+ * Command attribute.
+ *
+ * Command methods with this attribute can only be invoked by
+ * bot administrators. Apply to commands that should only be
+ * usable by trusted users.
+ * See_Also:
+ *    $(DPREF bot, Bot.addAdmins)
+ */
 struct admin {}
 
+/**
+ * Command attribute.
+ *
+ * Commands for command methods with attribute cannot be
+ * invoked in private messages, regardless of the
+ * value of the $(DPREF bot, Bot.allowPMCommands) property.
+ */
 struct channelOnly {}
